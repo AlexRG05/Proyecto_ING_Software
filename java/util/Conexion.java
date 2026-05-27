829c7751dee83f308f4e0aa2de002af3ac0ef8db
SatoOrozcoManuelMinoru <143605141+SatoOrozcoManuelMinoru@users.noreply.github.com>
2026-05-26 18:55:11 -0600

    Update Conexion.java

--- java/util/Conexion.java
+++ java/util/Conexion.java
@@ -3,6 +3,7 @@ package util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  *
@@ -10,24 +11,21 @@ import java.sql.SQLException;
  */
 public class Conexion {
 
-
-    private static final String URL =
-            "jdbc:sqlite:factory.db";
-
+    private static final String URL = "jdbc:sqlite:factory.db";
 
     public static Connection getConexion() {
-
-
         try {
-
-            return DriverManager.getConnection(
-                    URL);
-
+            Connection con = DriverManager.getConnection(URL);
+            Statement stmt = con.createStatement();
+            
+            // Magia: Creamos las tablas automáticamente si no existen
+            stmt.execute("CREATE TABLE IF NOT EXISTS proyectos (id INTEGER PRIMARY KEY, nombre TEXT NOT NULL, descripcion TEXT)");
+            stmt.execute("CREATE TABLE IF NOT EXISTS usuarios (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL, password TEXT NOT NULL)");
+            
+            return con;
+            
         } catch (SQLException e) {
-
-            System.out.println(
-                    e.getMessage());
-
+            System.out.println("Error de BD: " + e.getMessage());
             return null;
         }
     }
